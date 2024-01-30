f8990cf65a5d5c5133b25ac99126defd15310fc0
TheBoyz7 <nigerianguys7@gmail.com>
2024-01-30 20:47:26 +0100

    finishe linking001

---
+++ checkout/script.js
@@ -0,0 +1,4 @@
+function checkout() {
+    alert("logic shall be implemented as soon as backend is completed");
+    // Add your logic for processing the checkout, such as redirecting to a payment page, etc.
+}
